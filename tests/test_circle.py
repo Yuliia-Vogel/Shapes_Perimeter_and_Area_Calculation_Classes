dd87a77bd9e583df2f683fdb6007d7d235f1d3c2
Sergstm <sstm@ukr.net>
2024-11-23 16:47:43 +0200

    circle-area-test-added

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -2,8 +2,13 @@ from shapes.circle import Circle
 
 def test_perimeter():
     c = Circle()
-    c.parse(["Center", "1", "1", "Radius", "2"])
+    c.parse(["Center", "1", "1", "Radius", "3"])
     perimeter = c.get_perimeter()
-    assert perimeter == 12.57
+    assert perimeter == 18.85
 
 
+def test_area():
+    c = Circle()
+    c.parse(["Center", "1", "1", "Radius", "3"])
+    area = c.get_area()
+    assert area == 28.27
